9ef008b22f27510559a5d408e46768545e3da556
Dunkelweizen <cai.nowicki@gmail.com>
2020-12-02 21:41:48 -0500

    got print8 running!

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,22 @@
 
 import sys
 
+global HLT
+HLT = 0b0000001
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -61,5 +71,29 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        while self.pc < len(self.ram):
+            ir = self.pc
+            instruction = self.ram_read(ir)
+            try:
+                operand_a, operand_b = self.ram_read(ir + 1), self.ram_read(ir + 2)
+            except IndexError:
+                operand_a, operand_b = None, None
+
+            if instruction == HLT:
+                break
+            elif instruction == 0b10000010:
+                self.LDI(operand_a, operand_b)
+                self.pc += 1
+            elif instruction == 0b01000111:
+                self.PRN(operand_a)
+                self.pc += 1
+            else:
+                self.pc += 1
+
+
+
+    def LDI(self, address, value):
+        self.reg[address] = value
+
+    def PRN(self, address):
+        print(self.reg[address])
